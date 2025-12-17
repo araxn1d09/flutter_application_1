501381ea1044378f7b8639751d7ff9de6001e0ac
anig <mery241197@mail.ru>
2025-12-17 11:34:00 +0900

    LAB 2

--- lib/main.dart
+++ lib/main.dart
@@ -9,81 +9,85 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Мои картинки',
       home: ImageWrapScreen(),
     );
   }
 }
 
 class ImageWrapScreen extends StatelessWidget {
-  final List<String> imageUrls = [
-    'https://picsum.photos/200/200?random=1',
-    'https://picsum.photos/200/200?random=2',
-    'https://picsum.photos/200/200?random=3',
-    'https://picsum.photos/200/200?random=4',
-    'https://picsum.photos/200/200?random=5',
-    'https://picsum.photos/200/200?random=6',
+  // Пути к картинкам - БЕЗ папки assets!
+  final List<String> imagePaths = [
+    'images/car.jpg',
+    'images/cat.jpg', 
+    'images/dog.jpg',
+    'images/flower.jpg',
+    'images/house.jpg',
+    'images/tree.jpg',
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Картинки в Wrap'),
-        backgroundColor: Colors.blue,
+        title: Text('Картинки из папки images/'),
+        backgroundColor: Colors.deepPurple,
+        centerTitle: true,
       ),
       body: Container(
         color: Colors.grey[100],
-        padding: EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(20),
         child: Wrap(
-          spacing: 16.0,
-          runSpacing: 16.0,
-          children: imageUrls.map((url) {
+          spacing: 20.0,
+          runSpacing: 20.0,
+          alignment: WrapAlignment.center,
+          children: imagePaths.map((imagePath) {
             return Container(
-              margin: EdgeInsets.all(8.0),
+              margin: EdgeInsets.all(10.0),
               decoration: BoxDecoration(
                 color: Colors.white,
-                borderRadius: BorderRadius.circular(12.0),
+                borderRadius: BorderRadius.circular(15.0),
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 6.0,
-                    offset: Offset(0, 2),
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 10.0,
+                    offset: Offset(0, 5),
                   ),
                 ],
               ),
               child: ClipRRect(
-                borderRadius: BorderRadius.circular(12.0),
-                child: Image.network(
-                  url,
-                  width: 120,
-                  height: 120,
-                  fit: BoxFit.cover,
-                  loadingBuilder: (context, child, loadingProgress) {
-                    if (loadingProgress == null) return child;
-                    return Container(
-                      width: 120,
-                      height: 120,
-                      color: Colors.grey[300],
-                      child: Center(
-                        child: CircularProgressIndicator(),
-                      ),
-                    );
-                  },
-                  errorBuilder: (context, error, stackTrace) {
-                    return Container(
-                      width: 120,
-                      height: 120,
-                      color: Colors.grey[300],
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.error, color: Colors.red, size: 40),
-                          SizedBox(height: 8),
-                          Text('Ошибка', style: TextStyle(fontSize: 12)),
-                        ],
-                      ),
-                    );
-                  },
+                borderRadius: BorderRadius.circular(15.0),
+                child: SizedBox(
+                  width: 150,
+                  height: 150,
+                  child: Image.asset(
+                    imagePath,
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) {
+                      // Если картинка не найдена
+                      return Container(
+                        color: Colors.grey[200],
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.broken_image,
+                              size: 40,
+                              color: Colors.grey,
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              imagePath.split('/').last,
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
                 ),
               ),
             );
